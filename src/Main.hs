5416ca80b785c3d4a538385f71a18a4561692c8d
Danil <kaystd@gmail.com>
2019-06-23 20:40:46 +0700

    Add Scotty routes

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,18 @@
-helloWorld = "Hello World!"
+{-# LANGUAGE OverloadedStrings #-}
 
+import Web.Scotty
+
+import Data.Monoid (mconcat)
+
+main :: IO ()
 main = do
-  print helloWorld
+  scotty 3000 $ do
+    get "/register" $ do
+      html $ "<h1>Register</h1>"
+    get "/login" $ do
+      html $ "<h1>Login</h1>"
+    get "/users" $ do
+      html $ "<h1>Users</h1>"
+    get "/users/:user" $ do
+      user <- param "user"
+      html $ mconcat ["<h1>User ", user, "</h1>"]
