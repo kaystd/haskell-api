7e0d2b48bd8b9daef7672f5a586aae1ecc07e620
Danil <kaystd@gmail.com>
2019-06-28 16:45:38 +0700

    Add db utils

--- src/Db.hs
+++ src/Db.hs
@@ -4,6 +4,7 @@ module Db where
 
 import GHC.Generics (Generic)
 import Database.PostgreSQL.Simple
+import Data.Pool
 
 data DbConfig = DbConfig
   { dbName :: String
@@ -17,3 +18,27 @@ newConn conf = connect defaultConnectInfo
                        , connectPassword = dbPassword conf
                        , connectDatabase = dbName conf
                        }
+
+fetchSimple :: FromRow r => Pool Connection -> Query -> IO [r]
+fetchSimple pool sql = withResource pool retrieve
+  where retrieve conn = query_ conn sql
+
+fetch :: (FromRow r, ToRow q) => Pool Connection -> q -> Query -> IO [r]
+fetch pool args sql = withResource pool retrieve
+  where retrieve conn = query conn sql args
+
+execSql :: (ToRow q, FromRow r) => Pool Connection -> q -> Query -> IO [r]
+execSql pool args sql = withResource pool ins
+  where ins conn = query conn sql args
+
+fetchSimpleT :: FromRow r => Pool Connection -> Query -> IO [r]
+fetchSimpleT pool sql = withResource pool retrieve
+  where retrieve conn = withTransaction conn $ query_ conn sql
+
+fetchT :: (FromRow r, ToRow q) => Pool Connection -> q -> Query -> IO [r]
+fetchT pool args sql = withResource pool retrieve
+  where retrieve conn = withTransaction conn $ query conn sql args
+
+execSqlT :: (ToRow q, FromRow r) => Pool Connection -> q -> Query -> IO [r]
+execSqlT pool args sql = withResource pool ins
+  where ins conn = withTransaction conn $ query conn sql args
