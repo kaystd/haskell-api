16d6a90b3c54971ee312a134930bb22d60ea390a
Danil <kaystd@gmail.com>
2019-06-29 21:08:41 +0700

    Add DB CRUDs

--- src/Db.hs
+++ src/Db.hs
@@ -1,11 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
 
 module Db where
 import Domain
 
+import Web.Scotty.Internal.Types (ActionT)
 import GHC.Generics (Generic)
+import Control.Monad.IO.Class
 import Database.PostgreSQL.Simple
 import Data.Pool
+import Data.Hash.MD5
+import qualified Data.Text.Lazy as TL
 
 data DbConfig = DbConfig
   { dbName :: String
@@ -13,12 +18,16 @@ data DbConfig = DbConfig
   , dbPassword :: String
   } deriving (Show, Generic)
 
+-------------------------------------------------------------------------
+
 newConn :: DbConfig -> IO Connection
 newConn conf = connect defaultConnectInfo
                        { connectUser = dbUser conf
                        , connectPassword = dbPassword conf
                        , connectDatabase = dbName conf
                        }
+
+-------------------------------------------------------------------------
 
 fetchSimple :: FromRow r => Pool Connection -> Query -> IO [r]
 fetchSimple pool sql = withResource pool retrieve
@@ -43,3 +52,51 @@ fetchT pool args sql = withResource pool retrieve
 execSqlT :: (ToRow q, FromRow r) => Pool Connection -> q -> Query -> IO [r]
 execSqlT pool args sql = withResource pool ins
   where ins conn = withTransaction conn $ query conn sql args
+
+-------------------------------------------------------------------------
+
+findUserPassByLogin :: Pool Connection -> String -> IO (Maybe String)
+findUserPassByLogin pool login = do
+  res <- liftIO $ fetch pool (Only login) "SELECT password FROM user WHERE login = ?"
+  return $ password res
+    where password [(pwd)] = Just pwd
+          password _ = Nothing
+
+-------------------------------------------------------------------------
+
+userList :: Pool Connection -> IO [User]
+userList pool = do
+  res <- fetchSimple pool
+                     "SELECT login, userData, keyGost FROM user ORDER BY login ASC" {-:: IO [(TL.Text, TL.Text, TL.Text)]-}
+  return $ map (\(login, userData, keyGost) -> User login userData keyGost) res
+
+findUser :: Pool Connection -> TL.Text -> IO (Maybe User)
+findUser pool login = do
+  res <- fetch pool (Only login) "SELECT login, userData, keyGost FROM user WHERE login = ?"
+  return $ oneUser res
+
+addUser :: Pool Connection -> Maybe RegUser -> ActionT TL.Text IO (Maybe User)
+addUser pool Nothing = return Nothing
+addUser pool (Just (RegUser login password userData keyGost)) = do
+  res <- liftIO $ execSqlT pool [login, (TL.pack $ md5s $ Str $ TL.unpack password), userData, keyGost]
+                                "INSERT INTO user (login, password, userData, keyGost) VALUES (?, ?, ?, ?) RETURNING login userData keyGost"
+  return $ oneUser res
+
+updateUser :: Pool Connection -> Maybe User -> ActionT TL.Text IO (Maybe User)
+updateUser pool Nothing = return Nothing
+updateUser pool (Just (User login userData keyGost)) = do
+  res <- liftIO $ execSqlT pool [userData, keyGost, login]
+                                "UPDATE user SET userData = ?, keyGost = ? WHERE login = ? RETURNING login userData keyGost"
+  return $ oneUser res
+
+deleteUser :: Pool Connection -> TL.Text -> ActionT TL.Text IO (Maybe User)
+deleteUser pool login = do
+  res <- liftIO $ execSqlT pool [login]
+                                "DELETE FROM user WHERE login = ?"
+  return $ oneUser res
+
+-------------------------------------------------------------------------
+
+oneUser :: [(TL.Text, TL.Text, TL.Text)] -> Maybe User
+oneUser ((login, userData, keyGost) : _) = Just $ User login userData keyGost
+oneUser _ = Nothing
--- src/Domain.hs
+++ src/Domain.hs
@@ -5,20 +5,36 @@ module Domain where
 import Data.Text.Lazy
 import Data.Aeson
 
-data User = User Text Text Text Text
+data RegUser = RegUser Text Text Text Text
   deriving (Show)
+
+data User = User Text Text Text
+  deriving (Show)
+
+instance FromJSON RegUser where
+  parseJSON (Object v) = RegUser <$>
+                         v .: "login"     <*>
+                         v .: "password"  <*>
+                         v .: "userData"  <*>
+                         v .: "keyGost"
+
+instance ToJSON RegUser where
+  toJSON (RegUser login password userData keyGost) =
+    object [ "login"    .= login
+           , "password" .= password
+           , "userData" .= userData
+           , "keyGost"  .= keyGost
+           ]
 
 instance FromJSON User where
   parseJSON (Object v) = User <$>
                          v .: "login"     <*>
-                         v .: "password"  <*>
-                         v .: "userData"      <*>
+                         v .: "userData"  <*>
                          v .: "keyGost"
 
 instance ToJSON User where
-  toJSON (User login password userData keyGost) =
-    object [ "login" .= login
-           , "password" .= password
+  toJSON (User login userData keyGost) =
+    object [ "login"    .= login
            , "userData" .= userData
-           , "keyGost" .= keyGost
+           , "keyGost"  .= keyGost
            ]
